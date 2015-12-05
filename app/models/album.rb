fec68e160c5b88165ef82816127e37e3860aebb1
Eugen <Re4port@ukr.net>
2015-12-05 22:21:46 +0200

    add some validations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,8 @@
 class Album < ActiveRecord::Base
-	validates :album_name, length: { maximum: 30 }
+	validates :album_name,
+			 presence: true, #не пустая строка
+			 uniqueness: true, 
+			 length: { maximum: 30 }
+
 	has_many :tracks
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,6 @@
 class Track < ActiveRecord::Base
-	validates :record_name, length: { maximum: 40 }
-	validates :autor_name, length: { maximum: 40 }
+	validates :record_name, presence: true, uniqueness: true,
+				 length: { maximum: 40 }
+	validates :autor_name, presence: true, length: { maximum: 40 }
 	belongs_to :album
 end
