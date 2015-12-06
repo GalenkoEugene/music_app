5205acfa91a995ed72a07189e13c2cec31b3966f
Eugen <Re4port@ukr.net>
2015-12-06 23:15:28 +0200

    Finish static pages

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ActiveRecord::Base
+	#attr_accessiable :album_name  #По идее позволяет заполнять поля из консоли с помощью хеша) (Но я счастливик работает и без него)
 	validates :album_name,
 			 presence: true, #не пустая строка
 			 uniqueness: true, 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
+  #Добавление Capybara DSL во вспомогательный RSpec файл
+  config.include Capybara::DSL  #Для того чтобы правильно запустить тесты мы должны добавить строку в файл
 end
